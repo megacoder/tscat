5e419d59f27674a6e2d929e9b7a1dc428996e3d7
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-12-11 16:05:43 -0600

    Baseline

---
+++ src/tscat.c
@@ -0,0 +1,19 @@
+#include <tscat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <malloc.h>
+
+static	unsigned	nonfatal;
+
+int
+main(
+	int		argc,
+	char * *	argv,
+)
+{
+	return( nonfatal ? 1 : 0 );
+}
